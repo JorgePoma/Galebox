251216a4e88c17de8d1cb5396ec62bd145c9f9a6
Jorge <jorge.poma@unl.edu.ec>
2021-07-12 17:25:22 -0500

    Base

---
+++ Galebox-Backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5b149852a95f7ec700e06c526411b389'),
+    },
+  },
+});
---
+++ Galebox-Backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e1e46cd-635f-4264-9cbb-8a54fc651197'
+};
